b053c75eba3daea1be1b82861ef0b7481c7a2d34
Chatis Santos <chatis@afrointelligence.com>
2023-11-29 16:12:02 -0500

    feat: fixed dropdown bug

--- src/components/navigation/TopNav.tsx
+++ src/components/navigation/TopNav.tsx
@@ -1,10 +1,7 @@
-import React, { Fragment } from 'react';
-import { Menu, Transition } from '@headlessui/react';
 import { Bars3Icon } from '@heroicons/react/24/outline';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 import navigationItems from './navigationItems';
-import getUniqueId from '../../getUniqueId';
 import DropdownMenu from '../utility/DropdownMenu';
 import { UserCircleIcon } from '@heroicons/react/20/solid';
 
@@ -28,50 +25,16 @@ export default function TopNav() {
 
         {/* Mobile */}
         <div className="md:hidden flex justify-between items-center">
-          <div>
-            <Menu as="div" className="relative inline-block text-left">
-              {({ open }) => (
-                <>
-                  <Menu.Button>
-                    <Bars3Icon className="w-6 h-6 text-white" />
-                  </Menu.Button>
-                  <Transition
-                    show={open}
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items
-                      static
-                      className="absolute right-0 mt-2 w-48 bg-white dark:bg-slate-700 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-10"
-                    >
-                      <div className="py-1">
-                        {navigationItems.map((link) => (
-                          <Menu.Item key={getUniqueId()}>
-                            {({ active }) => (
-                              <Link
-                                to={link.href}
-                                className={`${
-                                  active
-                                    ? 'bg-slate-600 dark:bg-slate-500 text-white'
-                                    : 'text-gray-900 dark:text-white'
-                                } block px-4 py-2 text-sm`}
-                              >
-                                {link.name}
-                              </Link>
-                            )}
-                          </Menu.Item>
-                        ))}
-                      </div>
-                    </Menu.Items>
-                  </Transition>
-                </>
-              )}
-            </Menu>
+          <div className="flex items-center gap-2">
+            <DropdownMenu
+              position="left"
+              buttonContent={<Bars3Icon className="w-6 h-6 text-white" />}
+              items={navigationItems.map((item) => ({
+                href: item.href,
+                content: item.name,
+              }))}
+            />
+
             <Link to="/" className="text-white font-bold text-xl">
               <img className="w-24" src={logo} alt="logo" />
             </Link>
--- src/components/utility/DropdownMenu.tsx
+++ src/components/utility/DropdownMenu.tsx
@@ -9,22 +9,31 @@ type DropdownMenuItem = {
 type Props = {
   buttonContent: React.ReactNode;
   items: DropdownMenuItem[];
+  position?: 'left' | 'right';
 };
 
-export default function DropdownMenu({ items, buttonContent }: Props) {
+export default function DropdownMenu({
+  items,
+  buttonContent,
+  position = 'right',
+}: Props) {
   return (
     <Menu as="div" className="z-10 relative inline-block text-left">
-      <Menu.Button className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+      <Menu.Button className="inline-flex justify-center w-full text-sm font-medium text-gray-700 border border-gray-300 rounded-md shadow-sm">
         {buttonContent}
       </Menu.Button>
-      <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+      <Menu.Items
+        className={`absolute ${position}-0 w-56 mt-2 origin-top-right bg-white dark:bg-gray-700 border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
+      >
         {items.map((item, index) => (
           <Menu.Item key={index}>
             {({ active }) => (
               <a
                 href={item.href}
                 className={`${
-                  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
+                  active
+                    ? 'bg-slate-600 dark:bg-slate-500 text-white'
+                    : 'text-gray-900 dark:text-white'
                 } group flex items-center px-4 py-2 text-sm`}
               >
                 {item.content}
